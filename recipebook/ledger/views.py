f0555d6f4ad235ba90f9468979faac6a8aef7b18
seandominicyap-cpu <sean.dominic.yap@student.ateneo.edu>
2026-02-09 22:28:43 +0800

    Minor tweaks and corrections

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,3 +1,45 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+RECIPES_CONTEXT = {
+    "recipes": [
+        {
+            "name": "Recipe 1",
+            "ingredients": [
+                {"name": "tomato", "quantity": "3pcs"},
+                {"name": "onion", "quantity": "1pc"},
+                {"name": "pork", "quantity": "1kg"},
+                {"name": "water", "quantity": "1L"},
+                {"name": "sinigang mix", "quantity": "1 packet"}
+            ],
+            "link": "/recipe/1"
+        },
+        {
+            "name": "Recipe 2",
+            "ingredients": [
+                {"name": "garlic", "quantity": "1 head"},
+                {"name": "onion", "quantity": "1pc"},
+                {"name": "vinegar", "quantity": "1/2cup"},
+                {"name": "water", "quantity": "1 cup"}, # Corrected key from 'quanity'
+                {"name": "salt", "quantity": "1 tablespoon"},
+                {"name": "whole black peppers", "quantity": "1 tablespoon"},
+                {"name": "pork", "quantity": "1 kilo"}
+            ],
+            "link": "/recipe/2"
+        }
+    ]
+}
+
+def recipe_list(request):
+    return render(request, 'recipe_list.html', RECIPES_CONTEXT)
+
+def recipe_1(request):
+    # Pass only the first recipe's data
+    context = RECIPES_DATA["recipes"][0]
+    return render(request, 'recipe_detail.html', context)
+
+def recipe_2(request):
+    # Pass only the second recipe's data
+    context = RECIPES_DATA["recipes"][1]
+    return render(request, 'recipe_detail.html', context)
--- recipebook/recipebook/settings.py
+++ recipebook/recipebook/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wo&(007m432wwxbnzg-r7qy^pk_vy#wk#b*26w!g4*_nne)_%q'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ledger'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +59,7 @@ ROOT_URLCONF = 'recipebook.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -114,4 +119,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
