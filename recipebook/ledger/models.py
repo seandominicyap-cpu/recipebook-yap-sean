4bfa097cbbc2a34ab02f9c9f8fbffd72abcfa332
seandominicyap-cpu <sean.dominic.yap@student.ateneo.edu>
2026-02-19 14:03:43 +0800

    Created models

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Ingredient(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=255)
+
+class RecipeIngredient(models.Model):
+    quantity = models.CharField(max_length=255)
+    ingredient = models.ForeignKey(
+        Ingredient,
+        on_delete=models.CASCADE,
+        related_name="recipe")
+    
+    recipe = models.ForeignKey(
+        Recipe, 
+        on_delete=models.CASCADE,
+        related_name = "ingredients"
+    )
