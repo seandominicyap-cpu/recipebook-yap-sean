c1fb86bb0b2f3d774504c13ea86d032c00ffce52
seandominicyap-cpu <sean.dominic.yap@student.ateneo.edu>
2026-03-12 21:32:18 +0800

    Created RecipeImage model

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -56,3 +56,13 @@ class RecipeIngredient(models.Model):
         on_delete=models.CASCADE,
         related_name="ingredients"
     )
+
+
+class RecipeImage(models.Model):
+    image = models.ImageField(upload_to='images/', null=False, blank=False)
+    description = models.CharField(max_length=255)
+    recipe = models.ForeignKey(
+        'Recipe', on_delete=models.CASCADE, related_name='images')
+
+    def __str__(self):
+        return self.description
