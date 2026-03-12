1d18444d2ee0502d6b1179769c84bd60d22d8545
seandominicyap-cpu <sean.dominic.yap@student.ateneo.edu>
2026-03-12 22:33:31 +0800

    Fixed bug in admin.py

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
-from .models import Recipe, Ingredient, RecipeIngredient, Profile
+from .models import Recipe, Ingredient, RecipeIngredient, Profile, RecipeImage
 
 # Register your models here.
 
